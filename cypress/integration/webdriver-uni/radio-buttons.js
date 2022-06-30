453bfc76b0f2e4565c41c081c1324bd105fb6dc7
KanfiOri <96200020+KanfiOri@users.noreply.github.com>
2022-06-30 10:13:34 +0300

    day four changes

--- cypress/integration/webdriver-uni/radio-buttons.js
+++ cypress/integration/webdriver-uni/radio-buttons.js
@@ -10,4 +10,17 @@ describe("Verify radio buttons via webdriveruni", () => {
     cy.get("#radio-buttons").find("[type='radio']").first().check;
     cy.get("#radio-buttons").find("[type='radio']").eq(1).check;
   });
+
+  it("Validate the states of specific radio buttons", () => {
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get([(value = "lettuce")]).should("not.be.checked");
+    cy.get([(value = "cabbage")]).should("be.checked");
+
+    [(value = "cabbage")];
+    [(value = "punpkin")];
+  });
 });
